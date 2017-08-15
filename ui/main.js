237ce419b4ad18fd0bd6c096d403dc542c994881
faizy6641 <30418618+faizy6641@users.noreply.github.com>
2017-08-15 22:38:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,12 @@
 console.log('Loaded!');
 document.getElementById('main-text').innerHTML='I am a Functional Test Engineer';
+
+var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function(){
+    var interval=setInterval(moveRight,5);
+};
